0327ce250e7eaee896fb373c9f1d437547bccb3c
Cristian Fernando Santos Ribero <csantosr@unal.edu.co>
2020-11-13 21:53:47 -0500

    Restrictions on video save (exercise)

--- telerehabilitation_API/therapy/models/exercise.py
+++ telerehabilitation_API/therapy/models/exercise.py
@@ -3,12 +3,16 @@ from django.db import models
 from . import Routine
 
 
+def upload_exercise_video_to(instance, filename):
+    return 'exercise_{}/{}'.format(instance.id, filename)
+
+
 class Exercise(models.Model):
     routine = models.ForeignKey(Routine, on_delete=models.SET_NULL, null=True, related_name='exercises')
     name = models.CharField(max_length=128)
     description = models.TextField()
     order = models.IntegerField()
-    video = models.FileField()
+    video = models.FileField(upload_to=upload_exercise_video_to)
     has_been_tracked = models.BooleanField(default=False)
     is_model = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
--- telerehabilitation_API/therapy/views/exercise_view_set.py
+++ telerehabilitation_API/therapy/views/exercise_view_set.py
@@ -1,4 +1,4 @@
-from rest_framework import permissions, viewsets
+from rest_framework import permissions, viewsets, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
@@ -11,6 +11,13 @@ from telerehabilitation_API.therapy.serializers.exercise_video_serializer import
 class ExerciseViewSet(viewsets.ModelViewSet):
     queryset = Exercise.objects.all()
     serializer_class = ExerciseSerializer
+
+    def create(self, request, *args, **kwargs):
+        exercise_serialized = self.get_serializer(data=request.data)
+        exercise_serialized.is_valid(raise_exception=True)
+        self.perform_create(exercise_serialized)
+        headers = self.get_success_headers(exercise_serialized.data)
+        return Response(exercise_serialized.data, status=status.HTTP_201_CREATED, headers=headers)
 
     def get_permissions(self):
         permissions_classes = [permissions.IsAuthenticated]
@@ -26,12 +33,16 @@ class ExerciseViewSet(viewsets.ModelViewSet):
     def video(self, request, pk=None):
         exercise = self.get_object()
         video_serialized = ExerciseVideoSerializer(data=request.data)
-        if video_serialized.is_valid() and 'points' in request.data:
+        errors = []
+        if not video_serialized.is_valid():
+            errors.append({ 'video': video_serialized.error_messages})
+        if 'points' not in request.data:
+            errors.append({ 'points': 'There is no points' })
+        if not errors:
             for point in request.data['points'].split(','):
                 point_tracked = ExerciseSkeletonPointTracked(exercise=exercise, skeleton_point_id=int(point))
                 point_tracked.save()
             exercise.video = request.data['video']
             exercise.save()
             return Response(status=200)
-        else:
-            return Response({'errors': video_serialized.error_messages})
+        return Response({'errors': errors}, status=400)
