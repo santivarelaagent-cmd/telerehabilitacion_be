0280ea7fee813b0231ff73eb508c1bcaf97be683
Cristian Fernando Santos Ribero <csantosr@unal.edu.co>
2020-11-15 16:13:04 -0500

    Related name at exercise point tracked

--- telerehabilitation_API/therapy/models/exercise_skeleton_point_tracked.py
+++ telerehabilitation_API/therapy/models/exercise_skeleton_point_tracked.py
@@ -5,6 +5,6 @@ from telerehabilitation_API.therapy.models import SkeletonPoint, Exercise
 
 class ExerciseSkeletonPointTracked(models.Model):
     skeleton_point = models.ForeignKey(SkeletonPoint, on_delete=models.SET_NULL, null=True)
-    exercise = models.ForeignKey(Exercise, on_delete=models.SET_NULL, null=True)
+    exercise = models.ForeignKey(Exercise, on_delete=models.SET_NULL, null=True, related_name='tracked_points')
     max_angle = models.FloatField(null=True)
     min_angle = models.FloatField(null=True)
