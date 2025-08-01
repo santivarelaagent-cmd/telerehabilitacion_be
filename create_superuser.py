1c63ee75023595948fae69f3240fb16e237e556d
santivarelaagent-cmd <santivarela.agent@gmail.com>
2025-08-01 18:41:03 -0500

    super user fixed

--- create_superuser.py
+++ create_superuser.py
@@ -1,6 +1,10 @@
 # create_superuser.py
 import os
 import django
+import environ
+
+environ.Env.read_env()
+env = environ.Env()
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "telerehabilitation_API.settings")
 django.setup()
@@ -8,8 +12,13 @@ django.setup()
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
-if not User.objects.filter(username="admin").exists():
-    User.objects.create_superuser("admin", "admin@example.com", "123456789")
-    print("Superuser 'admin' creado con contraseña 123456789")
+
+username = env("DJANGO_SUPERUSER_USERNAME", default="admin")
+email = env("DJANGO_SUPERUSER_EMAIL", default="admin@example.com")
+password = env("DJANGO_SUPERUSER_PASSWORD", default="123456789")
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, email, password)
+    print(f"Superuser '{username}' creado con contraseña '{password}'")
 else:
-    print("El superuser 'admin' ya existe.")
+    print(f"El superuser '{username}' ya existe.")
